7a422841ef1a273330c0c768e4159633780deefd
Corey Reichle <corey.reichle@gmail.com>
2015-02-03 08:28:16 -0500

    More path cleanup

--- debian-demo.sh
+++ debian-demo.sh
@@ -12,13 +12,16 @@
 #################################################################
 
 cat << _EOF
-This script preps a Debian-based machine to host some demo applications for a mesh node. It should work on any
-Debian based distro, but tested against the current Debian and Ubuntu distros.
+This script preps a Debian-based machine to host some demo applications for a 
+mesh node. It should work on any Debian based distro, but tested against the
+current Debian and Ubuntu distros.
 
-This script must be executed using sudo or as root.  Networking must already be configured, either via wifi or eth0.
-Eth0 may be more "wow" factor, but it really doesn't matter.  Apache will listen on all ports.
+This script must be executed using sudo or as root.  Networking must already be
+configured, either via wifi or eth0. Eth0 may be more "wow" factor, but it
+really doesn't matter.  Apache will listen on all ports.
 
-If you already have an Apache instance set up, DO NOT RUN THIS.  It will likely clobber your web content.  You can
+If you already have an Apache instance set up, DO NOT RUN THIS.  It will likely
+clobber your web content.  You can
 do the bulk of the work by cloning in the appropriate repos:
 
 cd /var/www
@@ -47,8 +50,8 @@ git clone https://github.com/BuffaloMeshDotNet/website.git
 sed -i.bak -e '7d;8d' /var/www/html/website/local/config.php
 echo "Chat..."
 git clone https://github.com/BuffaloMeshDotNet/webchat.git
-touch /var/www/webchat/msg.html
-chmod ugo+rw /var/www/webchat/msg.html
+touch /var/www/html/webchat/msg.html
+chmod ugo+rw /var/www/html/webchat/msg.html
 echo "Library..."
 git clone https://github.com/BuffaloMeshDotNet/library.git
 
